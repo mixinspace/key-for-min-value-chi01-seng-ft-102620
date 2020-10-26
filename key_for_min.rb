7d00569f74afd59d5ecb440afa03770daee9a6cf
Islam Madkaur <islammadkaur@gmail.com>
2020-10-26 02:37:42 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,7 +6,7 @@ def key_for_min_value(name_hash)
 
 
 lowest_key= name_hash.reduce do |key, value|
-  key.last >value.last ? value : key
+  key.last > value.last ? value : key
 end
 if name_hash == {}
   lowest_key
